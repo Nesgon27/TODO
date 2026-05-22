e3cd399a7524a674c4db4f847d111156193a854f
Nestor Mendez <nestor_mg27@outlook.com>
2026-05-22 14:18:23 +0200

    Hello

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ export default function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <h1>Hello Users!</h1>
+        <h1>Hello Users</h1>
       </header>
       <div className="input-container">
         <div className="input-container-item">
